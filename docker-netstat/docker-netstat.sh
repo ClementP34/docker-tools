86747d2959d95755d16443d7dd68291aa17cc5a0
Clement P <galiper34@gmail.com>
2017-08-21 14:11:25 +0200

    Create docker-netstat.sh

---
+++ docker-netstat/docker-netstat.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#script equivalent a netstat pour les container docker
+#quand netstat est introuvable
+#pour utiliser, ./doccker-netstat.sh container_name_ou_id
+#Necessite les droits root
+
+PID_CONTAINER=`docker inspect -f '{{.State.Pid}}' ${1}`
+nsenter -t $PID_CONTAINER -n netstat -paunt
